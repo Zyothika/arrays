f065f5c6a8294f0bc6592eceb0cfdeece039149d
Zyothika <127111482+Zyothika@users.noreply.github.com>
2023-07-19 21:00:36 +0530

    Add files via upload

---
+++ move_negative _numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;   
+int main()
+{
+    int n, arr[10];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++)
+    {
+        cout<<" "<<arr[i];
+    }
+}
